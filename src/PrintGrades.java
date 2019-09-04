eb50927646088ab0f4bec212b96cb99e654d060f
Beza25 <sirak.bezawit@gmail.com>
2019-09-04 16:42:07 -0400

    Second commit

--- src/PrintGrades.java
+++ src/PrintGrades.java
@@ -1,37 +1,48 @@
 import java.util.Scanner;
 
 public class PrintGrades {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String response;
 
+        do {
+            System.out.println("Enter your score: ");
+            int grade = input.nextInt();
+            input.nextLine();
 
-        System.out.println("Enter your exam score in number: ");
+            if (grade >= 97 && grade <= 100) {
+                System.out.println("Good Job. Your grade is A+.");
+            } else if (grade >= 94 && grade <= 96) {
+                System.out.println("Good Job. Your grade is A.");
+            } else if (grade >= 90 && grade <= 93) {
+                System.out.println("Good Job. Your grade is A-.");
+            } else if (grade >= 87 && grade <= 89) {
+                System.out.println("Good Job. Your grade is B+.");
+            } else if (grade >= 84 && grade <= 86) {
+                System.out.println("Good Job. Your grade is B.");
+            } else if (grade >= 80 && grade <= 83) {
+                System.out.println("Good Job. Your grade is B-.");
+            } else if (grade >= 77 && grade <= 79) {
+                System.out.println("Good Job. Your grade is C+.");
+            } else if (grade >= 70 && grade <= 74) {
+                System.out.println("Good Job. Your grade is C.");
+            } else if (grade >= 70 && grade <= 73) {
+                System.out.println("Good Job. Your grade is C-.");
+            } else if (grade >= 60 && grade <= 69) {
+                System.out.println("Your grade is D.");
+            } else {
+                System.out.println("Go back to School.");
+            }
+            System.out.println("Do you want to enter another score (Y/N): ");
+            response = input.nextLine();
+        } while (response.equalsIgnoreCase("Y"));
 
-        int grade= input.nextInt();
-
-       if (grade >=  97 && grade <= 100){
-           System.out.println("Good Job. Your grade is A+.");
-       }else if(grade >= 94 && grade <= 96){
-           System.out.println("Good Job. Your grade is A.");
-       }else if (grade >= 90 && grade <=93){
-           System.out.println("Good Job. Your grade is A-.");
-       }else if (grade >= 87 && grade <=89){
-           System.out.println("Good Job. Your grade is B+.");
-       }else if (grade >= 84 && grade <=86){
-           System.out.println("Good Job. Your grade is B.");
-       }else if (grade >= 80 && grade <= 83){
-           System.out.println("Good Job. Your grade is B-.");
-       }else if (grade >= 77 && grade <=79){
-           System.out.println("Good Job. Your grade is C+.");
-       }else if (grade >=74 && grade <= 74){
-           System.out.println("Good Job. Your grade is C.");
-       }else if(grade >= 70 && grade <= 73 ){
-           System.out.println("Good Job. Your grade is C-.");
-       }else if (grade >= 60 && grade <= 69){
-           System.out.println("Your grade is D.");
-       }else {
-           System.out.println("Go back to School.");
-       }
-
+        System.out.println("Good Bye");
     }
+
+
+
+
+
+
 }
